89b82c461a9ee06ee165c1a31e5f0487deea9c30
roshansingha <roshansinghaagt@gmail.com>
2020-07-01 15:36:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Grid } from '@material-ui/core';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+import youtube from './api/youtube';
+
+class App extends React.Component {
+
+    handleSubmit = async(searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyDAvv6bjZ_l4q8A66AxJyASr6-lY_CUt40',
+                q: searchTerm,
+            }
+        });
+
+        console.log(response.data.items);
+    }
+
+    render() {
+        return (
+            <Grid justify="center" container spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs ={8}>
+                            <VideoDetail/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* Video List*/}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoDetail = () => {
+    return(
+        <h1> Video detail Component</h1>
+    )
+}
+
+export default VideoDetail;
